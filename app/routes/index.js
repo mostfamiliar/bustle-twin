06483be330bb723a5d269792a1fc7fa09de7553a
torrence stratton <tlstratton@gmail.com>
2016-03-29 17:11:47 -0700

    add carousel but can't get display working correctly

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,19 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    loading: function () {
+       // Add the active classes (required by the carousel to work)
+       Ember.$('.carousel-inner div.item').first().addClass('active');
+       Ember.$('.carousel-indicators li').first().addClass('active');
+       // Set the values of data-slide-to attributes
+       Ember.$('.carousel-indicators li').each(function (index, li) {
+           Ember.$(li).attr('data-slide-to', index);
+       });
+       console.log("working");
+       // Start the carousel
+      //  Ember.$('.carousel').carousel();
+       return true;
+   },
    save3(params) {
      var newStory = this.store.createRecord('story', params);
      newStory.save();
